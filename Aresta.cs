c6394df64173c934b4d9b262f410a9b1fd5981f0
Fernando Milano <131847205+zSkorp@users.noreply.github.com>
2023-12-14 19:53:42 -0300

    Add files via upload

---
+++ Aresta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2WF
+{
+    internal class Aresta
+    {
+        private double aresta;
+
+
+        public Aresta(double aresta)
+        {
+            this.aresta = aresta;
+        }
+
+        public Aresta()
+        {
+            this.aresta = 0;
+        }
+
+        public void setAresta(double aresta)
+        {
+            this.aresta = aresta;
+        }
+
+        public double getAresta()
+        {
+            return this.aresta = aresta;
+        }
+
+
+        public double CalcularArea()
+
+        {
+            return aresta * aresta;
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace EX2WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtAresta = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(358, 128);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtAresta
+            // 
+            this.txtAresta.Enabled = false;
+            this.txtAresta.Location = new System.Drawing.Point(358, 263);
+            this.txtAresta.Name = "txtAresta";
+            this.txtAresta.Size = new System.Drawing.Size(100, 20);
+            this.txtAresta.TabIndex = 2;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.Color.Snow;
+            this.lbl1.Location = new System.Drawing.Point(369, 112);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(81, 13);
+            this.lbl1.TabIndex = 3;
+            this.lbl1.Text = "Digite a aresta :";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.MistyRose;
+            this.btnCalcular.Location = new System.Drawing.Point(372, 219);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txtAresta);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Calcular Area";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtAresta;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
